cb57e8d4de54064cfcc84134ed28e5c9404ed120
jeffwei2019 <jifft@126.com>
2019-04-10 16:06:13 +0800

    remote target directory

--- target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-insert into user(username,password,age,create_time) values('jeff','123',25,123124);
-insert into user(username,password,age,create_time) values('tony','123',30,123124);
-insert into user(username,password,age,create_time) values('androw','123',40,123124);
--- target/classes/schema.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists `user`;
-create table user(
-	id int IDENTITY primary key,
-	username varchar(100),
-	password varchar(100),
-	age int,
-	create_time long
-);
